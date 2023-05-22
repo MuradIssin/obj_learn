54f4b68204e61cdce0b3e5e073fff9e487118f17
MuradIssin <117025448+MuradIssin@users.noreply.github.com>
2023-05-22 11:33:01 +0600

    Update tasks01.js

--- tasks01.js
+++ tasks01.js
@@ -1,24 +1,23 @@
 'use strict';
 const rainbow = 'padding: 10px 5px; margin-bottom: 6px; border: 1px solid blue; text-shadow: 1px 1px 2px white,  0 0 0.8; color: orange';
-console.log('%c Модуль 4 урок 5 ', rainbow);
+console.log('%c Модуль 4 урок 6 ', rainbow);
 
 console.warn('Первая задача');
 
-// Первая задача:
-// Выведите в консоль таблицу степеней от 1 до 10 по аналогии как в уроке выводится таблица умножения
+// массивы
 
 
-const a = 10;
-const m = 10;
+const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];
+const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
 
-for (let i = 1; i <= a; i++) {
-  let stringPrint = '';
-  let val;
-  for (let j = 1; j <= m; j++) {
-    val = i ** j + '';
-    stringPrint += `|${' '.repeat(11 - val.length)} ${i ** j} `;
+const filter = (all, failed) => {
+  const passedArr = [];
+  for (const elem of all) {
+    if (!failed.includes(elem)) {
+      passedArr.push(elem);
+    }
   }
-  stringPrint += `|`
-  console.log(stringPrint);
+  return passedArr
 }
 
+console.log(`Список студентов сдавших экзамен: ${filter(allStudents, failedStudents)}`);
