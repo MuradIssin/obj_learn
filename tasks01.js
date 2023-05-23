6a67ded426aaaf97889b0d775acb3c4f0013978b
MuradIssin <117025448+MuradIssin@users.noreply.github.com>
2023-05-23 18:54:39 +0600

    Update tasks01.js

--- tasks01.js
+++ tasks01.js
@@ -1,23 +1,21 @@
 'use strict';
 const rainbow = 'padding: 10px 5px; margin-bottom: 6px; border: 1px solid blue; text-shadow: 1px 1px 2px white,  0 0 0.8; color: orange';
-console.log('%c Модуль 4 урок 6 ', rainbow);
+console.log('%c Модуль 4 урок 7 ', rainbow);
 
-console.warn('Первая задача');
+console.warn('Задание1 = Первая задача');
 
 // массивы
 
+// const arrayRandom = Array(100);
 
-const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];
-const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
-
-const filter = (all, failed) => {
-  const passedArr = [];
-  for (const elem of all) {
-    if (!failed.includes(elem)) {
-      passedArr.push(elem);
-    }
+const arrayRandom = (arrayLength = 5) => {
+  const startNumber = 1;
+  const finishNumber = 100;
+  const newArray = [];
+  for (let i = 0; i < arrayLength; i++) {
+    newArray.push(Math.round(Math.random() * (finishNumber - startNumber + 1)))
   }
-  return passedArr
+  return newArray
 }
 
-console.log(`Список студентов сдавших экзамен: ${filter(allStudents, failedStudents)}`);
+console.log(`массив из случайных чисел ${arrayRandom(100)}`);
