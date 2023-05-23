eeba1edc17fcd31c891c6edc09572439b6d9fba5
MuradIssin <117025448+MuradIssin@users.noreply.github.com>
2023-05-23 18:56:33 +0600

    Update tasks03.js

--- tasks03.js
+++ tasks03.js
@@ -2,21 +2,39 @@
 // const rainbow = 'padding: 10px 5px; margin-bottom: 6px; border: 1px solid blue; text-shadow: 1px 1px 2px white,  0 0 0.8; color: orange';
 // console.log('%c Модуль 4 урок 4 ', rainbow);
 
-console.warn('Третья задача');
+console.warn('Задание1 - Третья задача');
 
 // Третья задача:
 
 
+const arrayRandom3 = (arrayLength = 5, m = -100, n = 100, evenOrOdd) => {
+  const startNumber = Math.min(n, m);
+  const finishNumber = Math.max(n, m);
 
-const names = ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
-
-const addPrefix = (nameIn, prefix) => {
-  const newNames = [];
-  for (const elem of nameIn) {
-    newNames.push(prefix + ' ' + elem)
+  if (evenOrOdd.toLowerCase() != "even" && evenOrOdd.toLowerCase() != "odd") {
+    return "не был корректно введен even или odd"
   }
-  return newNames;
+
+  const newArray = [];
+
+  for (let i = 0; i < arrayLength; i++) {
+    let newNumber = 0;
+    let tryAgain = true;
+    do {
+      let newNumber = Math.round(Math.random() * (finishNumber - startNumber + 1)) + startNumber;
+      if ((newNumber % 2 != 0) && evenOrOdd.toLowerCase() === 'odd') {
+        newArray.push(newNumber);
+        tryAgain = false;
+      }
+      if ((newNumber % 2 === 0) && evenOrOdd.toLowerCase() === 'even') {
+        newArray.push(newNumber);
+        tryAgain = false;
+      }
+    } while (tryAgain)
+    // newArray.push(Math.round(Math.random() * (finishNumber - startNumber + 1)) + startNumber)
+  }
+  return newArray;
 }
 
-console.log(`Входящий список ${names} результат ${addPrefix(names, 'Mr')}`)
 
+console.log(`массив из случайных чисел ${arrayRandom3(100, -100, 100, 'even')}`);
