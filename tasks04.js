3e28aeb266c4214fd2eed6b4cf8fd17e798e3d77
MuradIssin <117025448+MuradIssin@users.noreply.github.com>
2023-05-23 18:57:07 +0600

    Update tasks04.js

--- tasks04.js
+++ tasks04.js
@@ -1,29 +1,24 @@
 // 'use strict';
 // const rainbow = 'padding: 10px 5px; margin-bottom: 6px; border: 1px solid blue; text-shadow: 1px 1px 2px white,  0 0 0.8; color: orange';
-// console.log('%c Модуль 4 урок 4 ', rainbow);
+// console.log('%c Модуль 4 урок 7 ', rainbow);
 
-console.warn('не обязательное задание ');
+console.warn('Задание1  - не обязательное задание ');
 
 // 4 задача:
 
-const allCashboxNew = [
-  [12, 4500],
-  [7, 3210],
-  [4, 650],
-  [3, 1250],
-  [1, 990],
-  [6, 13900],
-  [1, 370],
-];
+const leapYear = (m = 2000, n = 2024) => {
+  const startYear = Math.min(n, m);
+  const finishYear = Math.max(n, m);
 
-const getAveragePriceGoods = allCash => {
-  let goods = 0;
-  let summaGoods = 0;
-  for (const elem of allCash) {
-    goods += elem[0];
-    summaGoods += elem[1];
+  const newArray = [];
+
+  for (let i = startYear; i <= finishYear; i++) {
+    if (i % 4 === 0) {
+      newArray.push(i);
+    }
   }
-  return Math.round(summaGoods / goods * 100) / 100
+  return newArray;
 }
 
-console.log(`средняя стоимость одного товара: ${getAveragePriceGoods(allCashboxNew)}`)
+
+console.log(`Високосный год ${leapYear(1900, 2024)}`);
