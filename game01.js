0ee741e6b745521044a103df02e329852d40c23b
MuradIssin <117025448+MuradIssin@users.noreply.github.com>
2023-05-24 07:20:32 +0600

    Update game01.js

--- game01.js
+++ game01.js
@@ -5,20 +5,33 @@ const guessNumber = (Math.round(Math.random() * 99) + 1);
 
 // console.log(`загаданное число ${guessNumber}`);
 let answer;
-answer = prompt("угадай число от 1 до 100");
-while (guessNumber != +answer) {
+let hint = '';
+
+while (guessNumber !== +answer) {
+
+  answer = prompt(`${hint}угадай число от 1 до 100`);
+
   if (answer === null) break;
-  if (+answer > guessNumber) {
-    answer = prompt("Меньше!, введите новый вариант");
-    if (answer === null) break;
-  }
-  if (+answer < guessNumber) {
-    answer = prompt("Больше>!, введите новый вариант");
-    if (answer === null) break;
+
+  // console.log(typeof +answer);
+  if (Number.isFinite(+answer)) {
+    // console.log("число")
+
+    if (+answer > guessNumber) {
+      hint = 'Меньше! ';
+      // answer = prompt("Меньше!, введите новый вариант");
+      // if (answer === null) break;
+    } else if (+answer < guessNumber) {
+      hint = 'Больше>! ';
+      // answer = prompt("Больше>!, введите новый вариант");
+      // if (answer === null) break;
+    }
+  } else {
+    hint = 'Введи число! ';
   }
 }
 
-if ((answer != null) && (guessNumber === +answer)) {
+if ((answer !== null) && (guessNumber === +answer)) {
   console.log(`Правильно! ${guessNumber}`)
 }
 else {
